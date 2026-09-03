4efa1b1eeda8aa9705fd9d9083f4adb532578492
Jonah Lewis <lewisj41@unlv.nevada.edu>
2026-09-02 19:23:41 -0500

    add decaf and darkRoast coffee

---
+++ Decorator/Starbuzz/DarkRoast.java
@@ -0,0 +1,11 @@
+public class DarkRoast extends Beverage{
+
+    public DarkRoast() {
+        description = "House Blend Coffee";
+    }
+
+    @Override
+    public double cost() {
+        return 0;
+    }
+}
---
+++ Decorator/Starbuzz/Decaf.java
@@ -0,0 +1,11 @@
+public class Decaf extends Beverage{
+
+    public Decaf() {
+        description = "Dark Roast Coffee";
+    }
+
+    @Override
+    public double cost() {
+        return 1.50;
+    }
+}
