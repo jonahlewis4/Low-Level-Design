17b4ba7e10b9d83d816e4a1ab7a65c64a7de933f
Jonah Lewis <lewisj41@unlv.nevada.edu>
2026-09-03 20:14:19 -0500

    refactor pizza creation to subclass

--- Factory/Pizza/PizzaStore.java
+++ Factory/Pizza/PizzaStore.java
@@ -1,14 +1,13 @@
-public class PizzaStore {
-    SimplePizzaFactory factory;
-    public PizzaStore(SimplePizzaFactory factory) {
-        this.factory = factory;
-    }
+public abstract class PizzaStore {
+
     Pizza orderPizza(String type) {
-        Pizza pizza = factory.CreatePizza(type);
+        Pizza pizza = createPizza(type);
         pizza.prepare();
         pizza.bake();
         pizza.cut();
         pizza.box();
         return pizza;
     }
+
+    abstract Pizza createPizza(String type);
 }
