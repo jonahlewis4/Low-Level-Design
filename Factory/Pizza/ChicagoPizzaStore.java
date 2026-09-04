1c8879608c2bb45a7576b93d539179a8642b8b67
Jonah Lewis <lewisj41@unlv.nevada.edu>
2026-09-03 20:16:10 -0500

    add ChicagoPizzaStore

---
+++ Factory/Pizza/ChicagoPizzaStore.java
@@ -0,0 +1,11 @@
+public class ChicagoPizzaStore extends PizzaStore{
+    Pizza createPizza(String item) {
+        return switch (item) {
+            case "cheese" -> new ChicagoStyleCheesePizza();
+            case "veggie" -> new ChicagoStyleVeggiePizza();
+            case "clam" -> new ChicagoStyleClamPizza();
+            case "pepperoni" -> new ChicagoStylePepperoniPizza();
+            default -> null;
+        };
+    }
+}
