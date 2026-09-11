5251365528310634ed73f46f99cf32dce35dd000
Jonah Lewis <lewisj41@unlv.nevada.edu>
2026-09-10 22:07:03 -0500

    add Stereo

--- Command/Remote/Light.java
+++ Command/Remote/Light.java
@@ -2,4 +2,8 @@ public class Light {
     public void on() {
         //does nothing
     }
+
+    public void off() {
+        //does nothing
+    }
 }
---
+++ Command/Remote/Stereo.java
@@ -0,0 +1,8 @@
+public class Stereo {
+    void on(){}
+    void off() {}
+    void setCd(){}
+    void setDvd() {}
+    void setRadio() {}
+    void setVolume(int volume) {}
+}
