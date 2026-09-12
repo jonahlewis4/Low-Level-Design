81103daf95231a0d0bb3c51075f05e1715439755
Jonah Lewis <lewisj41@unlv.nevada.edu>
2026-09-11 20:33:59 -0500

    add a MacroCommand and implementation example

---
+++ Command/Remote/MacroCommand.java
@@ -0,0 +1,18 @@
+public class MacroCommand implements Command{
+    Command[] commands;
+    public MacroCommand(Command[] commands) {
+        this.commands = commands;
+    }
+
+    public void execute() {
+        for(Command command : commands) {
+            command.execute();
+        }
+    }
+
+    public void undo() {
+        for(Command command : commands) {
+            command.undo();
+        }
+    }
+}
--- Command/Remote/RemoteLoader.java
+++ Command/Remote/RemoteLoader.java
@@ -1,22 +1,30 @@
 public class RemoteLoader {
     public static void main(String[] args) {
-        RemoteControlWithUndo remoteControl = new RemoteControlWithUndo();
-        CeilingFan ceilingFan = new CeilingFan("Living Room");
-        CeilingFanMediumCommand ceilingFanMedium =
-                new CeilingFanMediumCommand(ceilingFan);
-        CeilingFanHighCommand ceilingFanHigh =
-                new CeilingFanHighCommand(ceilingFan);
-        CeilingFanOffCommand ceilingFanOff =
-                new CeilingFanOffCommand(ceilingFan);
+        Light light = new Light("Living Room");
+        Light tv = new Light("Living Room TV");
+        Stereo stereo = new Stereo("Living Room");
 
-        remoteControl.setCommand(0, ceilingFanMedium, ceilingFanOff);
-        remoteControl.setCommand(1, ceilingFanHigh, ceilingFanOff);
+
+        LightOnCommand lightOn = new LightOnCommand(light);
+        StereoOnWithCDCommand stereoOn = new StereoOnWithCDCommand(stereo);
+        LightOnCommand tvOn = new LightOnCommand(tv);
+
+        LightOffCommand lightOff = new LightOffCommand(light);
+        StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+        LightOffCommand tvOff = new LightOffCommand(tv);
+
+        Command[] partyOn = {lightOn, stereoOn, tvOn};
+        Command[] partyOff = {lightOff, stereoOff, tvOff};
+
+        MacroCommand partyOnMacro = new MacroCommand(partyOn);
+        MacroCommand partyOffMacro = new MacroCommand(partyOff);
+
+        RemoteControlWithUndo remoteControl = new RemoteControlWithUndo();
+        remoteControl.setCommand(0, partyOnMacro, partyOffMacro);
+        System.out.println(remoteControl);
+        System.out.println("--- Pushing Macro On---");
         remoteControl.onButtonWasPushed(0);
+        System.out.println("--- Pushing Macro Off---");
         remoteControl.offButtonWasPushed(0);
-        System.out.println(remoteControl);
-        remoteControl.undoButtonWasPushed();
-        remoteControl.onButtonWasPushed(1);
-        System.out.println(remoteControl);
-        remoteControl.undoButtonWasPushed();
     }
 }
